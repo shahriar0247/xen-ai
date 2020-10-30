8684f5ecea72a50b71c5f7d4f5cc3120fc809ed0
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2020-10-31 00:25:20 +0600

    Add files via upload

--- requirements.bat
+++ requirements.bat
@@ -3,3 +3,4 @@ pip install pywin32
 pip install SpeechRecognition
 pip install pyaudio
 pip install flask
+pip install gTTS
--- say.py
+++ say.py
@@ -2,16 +2,51 @@ import win32com.client as wincl
 import pythoncom
 import time
 from pywintypes import com_error
+from gtts import gTTS 
+import os 
+import time
+from playsound import playsound
+from urllib.request import urlopen, URLError
 
+def say(text):
+    if internet_on():
+        say_online(text)
+    else:
+        say_offline(text)
+        
 
-def say(texttosay):
+def say_online(text):
+    gtts_object = gTTS(text=text)
+    set_temp_dir()
+    filename = "temp/" + str(time.time()) + ".mp3"
+    gtts_object.save(filename) 
+    playsound(filename) 
+
+def say_offline(text):
     pythoncom.CoInitialize()
     speak = wincl.Dispatch("SAPI.SpVoice")
     try:
-        speak.Speak(texttosay)
+        speak.Speak(text)
     except com_error:
         time.sleep(1)
-        speak.Speak(texttosay)
+        speak.Speak(text)
         
-        
+def set_temp_dir():
+    try:
+        if os.path.isdir("temp"):
+            return True
+        else:
+            os.mkdir("temp")
+            return True
+        return False
+    except Exception as e:
+        print(e)
+        return False
 
+
+def internet_on():
+    try:
+        urlopen('http://216.58.192.142', timeout=1)
+        return True
+    except URLError as err: 
+        return False
