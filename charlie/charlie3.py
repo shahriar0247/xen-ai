88dae8ead4bc7b312694fea3c7f219b12696579f
Ahmed Shahriar <shahriar0247@gmail.com>
2021-12-22 04:28:14 +0600

    asdasd

--- charlie/charlie3.py
+++ charlie/charlie3.py
@@ -8,10 +8,6 @@ def train(new_context):
         global context
         context = context + new_context
 
-context = """
-Alif is 20 years old, Conner is 29 years old
-"""
-
 def ask(question):
         result = question_answering(question=question, context=context)
         return result['answer']
