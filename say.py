f9ddff50e100aaa16f2a64fb8e09cce705801e6b
Ahmed Shahriar <shahriar0247@gmail.com>
2021-12-22 06:27:46 +0600

    now it talks fast

--- say.py
+++ say.py
@@ -16,6 +16,18 @@ import soundfile as sf
 import pyrubberband as pyrb
 import sys
 from pydub import AudioSegment
+from pydub.playback import play
+
+def speed_change(sound, speed=1.0):
+    # Manually override the frame_rate. This tells the computer how many
+    # samples to play per second
+    sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={
+         "frame_rate": int(sound.frame_rate * speed)
+      })
+     # convert the sound with altered frame rate to a standard frame rate
+     # so that regular playback programs will work right. They often only
+     # know how to play audio at standard frame rate (like 44.1k)
+    return sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
 
 
 
@@ -39,11 +51,11 @@ def say_online(text):
     print(22)
     gtts_object.save(filename)
     os.system("ffmpeg -i "+filename+" "+filename+".wav")
-
     filename = filename + ".wav"
-    filename = change_speed(filename)
+    print(filename)
+    sped_up_sound = AudioSegment.from_file(filename).speedup(1.3, 150, 25)
     print(23) 
-    playsound(filename) 
+    play(sped_up_sound)
     print(24)
 
 
